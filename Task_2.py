5d5e971e3e44748225c89a2509cf903f61f0378e
Yufan2023 <148704737+Yufan2023@users.noreply.github.com>
2023-10-30 01:12:43 -0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -125,7 +125,7 @@ def train_and_evaluate(model, train_images, train_labels, val_images, val_labels
     test_loss, test_accuracy = model.evaluate(test_images, test_labels)
 
     if model_name:
-        model.save(model_name)  # Save the model if a model name is provided
+        model.save(model_name)  
 
     return test_accuracy
 
